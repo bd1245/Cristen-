822eedfaaf439d8a6edc9e2626bb51bf69eb2dc1
bd1245 <100785245+bd1245@users.noreply.github.com>
2022-10-24 19:37:15 +0200

    Update Mq.py

--- Mq.py
+++ Mq.py
@@ -5,7 +5,7 @@ from Miq.التشغيل import arq
 async def main():
     await call_py.start()
     print("""    ------------------
-   | ميوزك كرستين  الان شغال ! |
+   | ميوزك ويسلون  الان شغال ! |
     ------------------"""    )
     await idle()
     await arq.close()
